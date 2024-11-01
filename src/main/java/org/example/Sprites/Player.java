6c79e3e2c4120b0c87389da78091abd47ccac170
Dylan Todd <dylantodd00@gmail.com>
2024-11-01 16:01:17 +0000

    Test

--- src/main/java/org/example/Sprites/Player.java
+++ src/main/java/org/example/Sprites/Player.java
@@ -4,32 +4,50 @@ import java.util.Locale;
 
 public class Player extends Sprite{
 
-    private int minColumn = 1;
-    private int minRow = 1;
-    private int maxColumn = board.getColumns();
-    private int maxRow = board.getRows();
+    private int score;
+    private final int maxColumn = board.getColumns();
+    private final int maxRow = board.getRows();
 
     public Player(int[] position) {
         super(position);
     }
 
-    public void move(String move) {
-        switch (move.toLowerCase()){
-            case "up":
-                int[] newPosition = {getPosition()[0] - 1, getPosition()[1]};
-                if (moveAllowed(newPosition)) {
-                    setPosition(newPosition);
-                }
-            case "down":
-                int[] newPosition = {getPosition()[0] + 1, getPosition()[1]};
-                if (moveAllowed(newPosition)) {
-                    setPosition(newPosition);
-                }
+    public int getScore() {
+        return score;
+    }
 
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void move(String move) {
+
+        if (move.equalsIgnoreCase("up")) {
+            int[] newPosition = {getPosition()[0] - 1, getPosition()[1]};
+            if (moveAllowed(newPosition)) {
+                setPosition(newPosition);
+            }
+        } else if (move.equalsIgnoreCase("down")) {
+            int[] newPosition = {getPosition()[0] + 1, getPosition()[1]};
+            if (moveAllowed(newPosition)) {
+                setPosition(newPosition);
+            }
+        } else if (move.equalsIgnoreCase("left")) {
+            int[] newPosition = {getPosition()[0], getPosition()[1] - 1};
+            if (moveAllowed(newPosition)) {
+                setPosition(newPosition);
+            }
+        } else if (move.equalsIgnoreCase("right")) {
+            int[] newPosition = {getPosition()[0], getPosition()[1] + 1};
+            if (moveAllowed(newPosition)) {
+                setPosition(newPosition);
+            }
         }
     }
 
     private boolean moveAllowed(int[] newPosition) {
+        int minColumn = 1;
+        int minRow = 1;
         return newPosition[0] >= minRow && newPosition[0] <= maxRow &&
                 newPosition[1] >= minColumn && newPosition[1] <= maxColumn;
     }
