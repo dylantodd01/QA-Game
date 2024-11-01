ea9faa9c69d85794f68b7cac717d891b25d0c18e
Dylan Todd <dylantodd00@gmail.com>
2024-11-01 15:27:40 +0000

    Worked on Player class

--- src/main/java/org/example/Sprites/Player.java
+++ src/main/java/org/example/Sprites/Player.java
@@ -1,4 +1,25 @@
 package org.example.Sprites;
 
-public class Player {
+import java.util.Locale;
+
+public class Player extends Sprite{
+
+    public Player(int[] position) {
+        super(position);
+    }
+
+    public void move(String move) {
+        switch (move.toLowerCase()){
+            case "up":
+                int[] newPosition = {getPosition()[0], getPosition()[1] - 1};
+                if (moveAllowed(newPosition)) {
+                    setPosition(newPosition);
+                }
+
+        }
+    }
+
+    private void moveAllowed(int[] newPosition) {
+    }
+
 }
