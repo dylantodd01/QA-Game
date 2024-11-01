7a19e7e083d2de059ab03ef39137edec380aaf61
Dylan Todd <dylantodd00@gmail.com>
2024-11-01 16:10:55 +0000

    Monster

--- src/main/java/org/example/Sprites/Monster.java
+++ src/main/java/org/example/Sprites/Monster.java
@@ -1,4 +1,23 @@
 package org.example.Sprites;
 
-public class Monster {
+public class Monster extends Sprite{
+
+    String type;
+
+    public Monster(int[] position) {
+        super(position);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String greeting() {
+        return "I am a " + type + " type of monster!";
+    }
 }
