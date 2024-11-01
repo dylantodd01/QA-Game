e58e14fe641ed5055c25765a72e4a69fd872f16f
Sarvesh-T16 <sarveshthaleshwar@gmail.com>
2024-11-01 16:03:47 +0000

    Added to Board Test

--- src/test/java/BoardTest/BoardTest.java
+++ src/test/java/BoardTest/BoardTest.java
@@ -1,4 +1,15 @@
 package BoardTest;
 
+import org.example.Board.Board;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class BoardTest {
+
+    @Test
+    public void testMinSizeBoardCreation(){
+        assertThrows(IllegalArgumentException.class,() -> {new Board(1, 3);});
+        assertThrows(IllegalArgumentException.class,() -> {new Board(4, 1);});
+    }
 }
