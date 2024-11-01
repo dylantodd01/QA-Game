dead32802ec90abf148d5d92c4255bb9270063fb
Sarvesh-T16 <sarveshthaleshwar@gmail.com>
2024-11-01 12:35:17 +0000

    Changes to Board made

--- src/main/java/org/example/Board/Board.java
+++ src/main/java/org/example/Board/Board.java
@@ -1,4 +1,17 @@
 package org.example.Board;
 
+import org.example.Sprites.Sprite;
+
+import java.sql.Array;
+
 public class Board {
+    private int rows;
+    private int columns;
+    private Sprite[][] boardArray ;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.boardArray = new Sprite[rows][columns];
+    }
 }
