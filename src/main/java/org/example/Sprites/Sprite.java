45c9d5e05760eb8df1f3fd8e71c915a9ef1ad8ef
Dylan Todd <dylantodd00@gmail.com>
2024-11-01 12:29:00 +0000

    Worked on Sprite class

--- src/main/java/org/example/Sprites/Sprite.java
+++ src/main/java/org/example/Sprites/Sprite.java
@@ -1,4 +1,44 @@
 package org.example.Sprites;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Sprite {
+
+    private String name;
+    private int[] position;
+
+    public Sprite(int[] position) {
+        this.position = position;
+        this.name = "Sprite";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public void setPosition(int[] position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Sprite{" +
+                "name='" + name + '\'' +
+                ", position=" + Arrays.toString(position) +
+                '}';
+    }
+
+    public String greeting() {
+        return "Hello!";
+    }
+
 }
